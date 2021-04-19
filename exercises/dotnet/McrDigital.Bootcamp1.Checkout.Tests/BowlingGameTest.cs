97d9a5100f9ee1786cd1101f1ee5a62b2e4a5169
ukales <audrey_lescot@waters.com>
2021-04-19 14:17:14 +0100

    Added test and code for spare

---
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout.Tests/BowlingGameTest.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+namespace McrDigital.Bootcamp1.Checkout.Tests
+{
+    public class BowlingGameTests
+    {
+        [Fact]
+        public void ScoreReturnsZero()
+        {
+            //Arrange
+            var game = new Game();
+            var rolls = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            //Act
+            var score = game.Score(rolls);
+
+            //Assert
+            Assert.Equal(0, score);
+
+        }
+
+          
+        [Fact]
+        public void WhenRollsArePlayedThenReturnsScore()
+        {
+            //Arrange
+            var game = new Game();
+            var rolls = new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+            //Act
+            var score = game.Score(rolls);
+
+            //Assert
+            Assert.Equal(20, score);
+
+        }
+
+        [Fact]
+        
+        public void ShouldCalculateScoreWhenSpareIsIncluded()
+        {
+            //Arrange
+            var game = new Game();
+            var rolls = new int[] { 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            //Act
+            var score = game.Score(rolls);
+
+            //Assert
+            Assert.Equal(12, score);
+
+        }
+
+    }
+
+       
+}
---
+++ exercises/dotnet/McrDigital.Bootcamp1.Checkout/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McrDigital.Bootcamp1.Checkout
+{
+    public class Game
+    {
+
+        public int Score(int [] rolls)
+        {
+            var score = 0;
+            
+
+            for (int i = 0; i < rolls.Length; i += 2)
+            {
+                var firstRoll = rolls[i];
+                var secondRoll = rolls[i + 1];
+
+                if ((firstRoll + secondRoll) == 10)
+                {
+                    score += rolls[i + 2]; //this is the first roll from the next frame
+                }
+                
+                score += firstRoll;
+                score += secondRoll;
+
+            }
+
+            return score;
+        }
+
+    }
+}
